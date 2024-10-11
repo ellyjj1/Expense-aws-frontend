744905c0ea27a49f16da2f5772a34811a12ea429
ellyjj1 <72015673+ellyjj1@users.noreply.github.com>
2024-10-12 10:06:14 +1300

    test on cloud environment

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -15,8 +15,8 @@ export default function GlobalState({ children }) {
     const [totalIncome, settotalIncome] = useState(0);
     const [allTransactions, setallTransactions] = useState([]);
 
-    // // Azure环境
-    // const baseURL = "https://expense-backend-ggc9a0gsgdd3c4a7.westus-01.azurewebsites.net/";
+    // Azure环境
+    const baseURL = "https://expense-backend-ggc9a0gsgdd3c4a7.westus-01.azurewebsites.net/";
 
     // Vercel环境
     // const baseURL = "https://expense-tracker-backend-navy.vercel.app/";
@@ -24,8 +24,8 @@ export default function GlobalState({ children }) {
     // //AWS环境
     // const baseURL = "https://54.206.71.113:8000/";
 
-    const baseURL = "http://127.0.0.1:8000/";
-    // // const baseURL_Auth = "http://127.0.0.1:8000/";
+    // const baseURL = "http://127.0.0.1:8000/";
+
 
 
 
